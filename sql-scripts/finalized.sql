d97d7e83ecff043c66a4d95a3f47db8471693f22
Ian Kinsey <ian@aikbix.com>
2025-10-12 01:40:29 -0400

    Finish internal rust functions

--- sql-scripts/finalized.sql
+++ sql-scripts/finalized.sql
@@ -1,15 +1,11 @@
 -- Make the rust encode_text an internal functuin
-ALTER FUNCTION sqlgen.encode_text(TEXT, TEXT) SET SCHEMA sqlgen_internal;
-REVOKE EXECUTE ON FUNCTION sqlgen_internal.encode_text(TEXT, TEXT) FROM public;
+ALTER FUNCTION sqlgen.internal_encode_text(TEXT, TEXT) SET SCHEMA sqlgen_internal;
+REVOKE EXECUTE ON FUNCTION sqlgen_internal.internal_encode_text(TEXT, TEXT) FROM public;
 
 -- Make the rust encode_text_batch an internal functuin
-ALTER FUNCTION sqlgen.encode_text_batch(TEXT, TEXT) SET SCHEMA sqlgen_internal;
-REVOKE EXECUTE ON FUNCTION sqlgen_internal.encode_text_batch(TEXT, TEXT[]) FROM public;
+ALTER FUNCTION sqlgen.internal_encode_text_batch(TEXT, TEXT) SET SCHEMA sqlgen_internal;
+REVOKE EXECUTE ON FUNCTION sqlgen_internal.internal_encode_text_batch(TEXT, TEXT[]) FROM public;
 
--- Make the rust decode_text an internal functuin
-ALTER FUNCTION sqlgen.decode_text(TEXT, TEXT) SET SCHEMA sqlgen_internal;
-REVOKE EXECUTE ON FUNCTION sqlgen_internal.decode_text(TEXT, TEXT) FROM public;
-
--- Make the rust decode_text_batch an internal functuin
-ALTER FUNCTION sqlgen.decode_text_batch(TEXT, TEXT) SET SCHEMA sqlgen_internal;
-REVOKE EXECUTE ON FUNCTION sqlgen_internal.decode_text_batch(TEXT, TEXT[]) FROM public;
+-- Make the rust instruct_text an internal functuin
+ALTER FUNCTION sqlgen.instruct_text(TEXT, TEXT) SET SCHEMA sqlgen_internal;
+REVOKE EXECUTE ON FUNCTION sqlgen_internal.instruct_text(TEXT, TEXT) FROM public;
--- src/api/internal.rs
+++ src/api/internal.rs
@@ -1,22 +1,56 @@
 use pgrx::pg_extern;
+use pgrx::prelude::*;
+use tokio::runtime::Runtime;
+
+use crate::stores::model_store::ModelStore;
+use crate::types::structs::instruct_message::InstructMessage;
+use crate::types::structs::instruct_message::InstructRole;
 
 // These functions lives in sqlgen_internal
+
 #[pg_extern]
-fn encode_text(model: &str, value: &str) -> &'static str {
-    unimplemented!()
+fn internal_encode_text(model: &str, value: &str) -> Vec<f32> {
+    let profile = ModelStore::get_model_profile(model).unwrap_or_else(|e| error!("{}", e));
+    let model = profile
+        .get_text_encoder_model()
+        .unwrap_or_else(|e| error!("{}", e));
+    let rt = Runtime::new().unwrap_or_else(|e| error!("failed to initialize runtime: {}", e));
+
+    rt.block_on(async { model.encode(value).await })
+        .unwrap_or_else(|e| error!("{}", e))
 }
 
 #[pg_extern]
-fn encode_text_batch(model: &str, values: Vec<String>) -> Vec<&'static str> {
-    unimplemented!()
+fn internal_batch_text_encode(model: &str, values: Vec<String>) -> Vec<Vec<f32>> {
+    let profile = ModelStore::get_model_profile(model).unwrap_or_else(|e| error!("{}", e));
+    let model = profile
+        .get_text_encoder_model()
+        .unwrap_or_else(|e| error!("{}", e));
+    let rt = Runtime::new().unwrap_or_else(|e| error!("failed to initialize runtime: {}", e));
+    let values: Vec<&str> = values.iter().map(|s| s.as_str()).collect();
+
+    rt.block_on(async { model.encode_many(&values).await })
+        .unwrap_or_else(|e| error!("{}", e))
 }
 
 #[pg_extern]
-fn decode_text(model: &str, value: &str) -> &'static str {
-    unimplemented!()
-}
+fn internal_instruct_text(model: &str, system_prompt: &str, user_prompt: &str) -> String {
+    let profile = ModelStore::get_model_profile(model).unwrap_or_else(|e| error!("{}", e));
+    let mut model = profile
+        .get_text_instruct_model()
+        .unwrap_or_else(|e| error!("{}", e));
+    let messages = vec![
+        InstructMessage {
+            role: InstructRole::System,
+            message: system_prompt.to_string(),
+        },
+        InstructMessage {
+            role: InstructRole::User,
+            message: user_prompt.to_string(),
+        },
+    ];
+    let rt = Runtime::new().unwrap_or_else(|e| error!("failed to initialize runtime: {}", e));
 
-#[pg_extern]
-fn decode_text_batch(model: &str, values: Vec<String>) -> Vec<&'static str> {
-    unimplemented!()
+    rt.block_on(async { model.get_assistant_response(messages).await })
+        .unwrap_or_else(|e| error!("{}", e))
 }
