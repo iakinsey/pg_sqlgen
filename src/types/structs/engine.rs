6eeddfd7719fbab34c42efa986fe550837f18b9f
Ian Kinsey <ian@aikbix.com>
2025-10-16 02:51:57 -0400

    Engine uses filter type

--- src/types/structs/engine.rs
+++ src/types/structs/engine.rs
@@ -1,4 +1,4 @@
-use pgrx::spi::SpiTupleTable;
+use pgrx::{spi::SpiTupleTable, PostgresEnum};
 
 use crate::{
     types::errors::ModelDriverError,
@@ -9,6 +9,26 @@ pub static DEFAULT_SYSTEM_PROMPT_TEMPLATE: &str = "TODO";
 pub static DEFAULT_USER_PROMPT_TEMPLATE: &str = "TODO";
 pub static DEFAULT_RELEVANT_TABLES_TEMPLATE: &str = "TODO";
 pub static DEFAULT_SIMILAR_QUERIES_TEMPLATE: &str = "TODO";
+pub static DEFAULT_FILTER_TABLES_TEMPLATE: &str = "TODO";
+
+#[derive(PostgresEnum, Eq, PartialEq)]
+pub enum TableFilterType {
+    Quick,
+    Smart,
+}
+
+impl TableFilterType {
+    pub fn from_str(val: &str) -> Result<Self, ModelDriverError> {
+        match val {
+            "quick" => Ok(Self::Quick),
+            "smart" => Ok(Self::Smart),
+            _ => Err(ModelDriverError::ParseError(format!(
+                "unable to parse filter type: {}",
+                val
+            ))),
+        }
+    }
+}
 
 // TODO update engine store to reflect new changes here
 pub struct TextToSqlEngine {
@@ -20,6 +40,8 @@ pub struct TextToSqlEngine {
     pub user_prompt_template: String,
     pub relevant_tables_template: String,
     pub similar_queries_template: String,
+    pub filter_tables_template: String,
+    pub filter_type: TableFilterType,
 }
 
 impl TextToSqlEngine {
@@ -43,11 +65,18 @@ impl TextToSqlEngine {
                 Some(v) => v,
                 None => DEFAULT_SIMILAR_QUERIES_TEMPLATE.to_string(),
             };
+        let filter_tables_template: String =
+            match get_column_optional(&row, "filter_tables_template")? {
+                Some(v) => v,
+                None => DEFAULT_FILTER_TABLES_TEMPLATE.to_string(),
+            };
 
         let name = get_column(&row, "name")?;
         let schema_name = get_column(&row, "schema_name")?;
         let encoder_model = get_column(&row, "encoder_model")?;
         let instruct_model = get_column(&row, "instruct_model")?;
+        let filter_type_str = get_column(&row, "filter_type")?;
+        let filter_type = TableFilterType::from_str(filter_type_str)?;
 
         Ok(Self {
             name,
@@ -58,6 +87,8 @@ impl TextToSqlEngine {
             user_prompt_template,
             relevant_tables_template,
             similar_queries_template,
+            filter_tables_template,
+            filter_type,
         })
     }
 }
