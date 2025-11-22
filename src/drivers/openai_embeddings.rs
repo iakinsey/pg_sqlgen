efd93558c4ef001b66275b129e84c4333199093a
Ian Kinsey <ian@aikbix.com>
2025-11-21 23:27:02 -0500

    Complete openai embeddings

--- src/drivers/openai_embeddings.rs
+++ src/drivers/openai_embeddings.rs
@@ -18,18 +18,25 @@ pub struct OpenAIEmbeddingsDriver {
 }
 
 #[derive(Serialize)]
-pub struct EmbeddingsRequest {
-    input: String,
-    model: String,
-    encoding_format: String,
+pub struct EmbeddingsRequest<'a> {
+    input: &'a str,
+    model: &'a str,
+    encoding_format: &'a str,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize)]
+pub struct BatchEmbeddingsRequest<'a> {
+    input: &'a [&'a str],
+    model: &'a str,
+    encoding_format: &'a str,
+}
+
+#[derive(Deserialize, Serialize)]
 pub struct EmbeddingsResponse {
     data: Vec<EmbeddingsData>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct EmbeddingsData {
     embedding: Vec<f32>,
 }
@@ -61,9 +68,9 @@ impl TextEncoderDriver for OpenAIEmbeddingsDriver {
         }
 
         let request_json = EmbeddingsRequest {
-            input: input.to_string(),
-            model: self.config.model.clone(),
-            encoding_format: "float".to_string(),
+            input: input,
+            model: &self.config.model,
+            encoding_format: "float",
         };
         let body = to_string(&request_json)?;
         let auth_header = get_auth_header(
@@ -72,7 +79,7 @@ impl TextEncoderDriver for OpenAIEmbeddingsDriver {
         );
         let req = self
             .client
-            .post(self.config.url.clone())
+            .post(&self.config.url)
             .body(body)
             .header("Content-Type", "application/json");
 
@@ -93,11 +100,14 @@ impl TextEncoderDriver for OpenAIEmbeddingsDriver {
 
         match from_str::<EmbeddingsResponse>(&text) {
             Ok(response) => {
-                let data = response.data.first().ok_or_else(|| {
-                    SqlgenError::ResponseError("no embeddings returned".to_string())
-                })?;
+                let embedding = response
+                    .data
+                    .into_iter()
+                    .next()
+                    .ok_or_else(|| SqlgenError::ResponseError("no embeddings returned".into()))?
+                    .embedding;
 
-                Ok(data.embedding.clone())
+                Ok(embedding)
             }
             Err(_) => Err(SqlgenError::ResponseError(format!(
                 "HTTP {}, failed to parse: {}",
@@ -107,7 +117,44 @@ impl TextEncoderDriver for OpenAIEmbeddingsDriver {
     }
 
     async fn encode_many(&self, inputs: &[&str]) -> Result<Vec<Vec<f32>>, SqlgenError> {
-        unimplemented!();
+        let request_json = BatchEmbeddingsRequest {
+            input: inputs,
+            model: &self.config.model,
+            encoding_format: "float",
+        };
+        let body = to_string(&request_json)?;
+        let auth_header = get_auth_header(
+            self.config.api_key.clone(),
+            self.config.authorization_type.clone(),
+        );
+        let req = self
+            .client
+            .post(&self.config.url)
+            .body(body)
+            .header("Content-Type", "application/json");
+
+        let req = match auth_header {
+            Some(k) => req.header("Authorization", k),
+            None => req,
+        };
+        let resp = req.send().await?;
+        let status = resp.status();
+        let text = resp.text().await?;
+
+        if !status.is_success() {
+            return Err(SqlgenError::ResponseError(format!(
+                "HTTP {}: {}",
+                status, text
+            )));
+        }
+
+        match from_str::<EmbeddingsResponse>(&text) {
+            Ok(response) => Ok(response.data.into_iter().map(|d| d.embedding).collect()),
+            Err(_) => Err(SqlgenError::ResponseError(format!(
+                "HTTP {}, failed to parse: {}",
+                status, text
+            ))),
+        }
     }
 }
 
@@ -115,6 +162,128 @@ impl TextEncoderDriver for OpenAIEmbeddingsDriver {
 mod tests {
     use super::*;
     use httpmock::{Method::POST, MockServer};
+
+    #[tokio::test]
+    async fn test_encode_batch() {
+        let response = EmbeddingsResponse {
+            data: vec![
+                EmbeddingsData {
+                    embedding: vec![0.1, 0.2, 0.3],
+                },
+                EmbeddingsData {
+                    embedding: vec![1.1, 1.2, 1.3],
+                },
+                EmbeddingsData {
+                    embedding: vec![2.1, 2.2, 2.3],
+                },
+            ],
+        };
+        let encode_response = to_string(&response).unwrap();
+        let url_part = "/v1/embeddings";
+        let model_name = "test-model-name";
+        let inputs = vec!["test-input-1", "test-input-2", "test-input-3"];
+        let api_key = "api-key";
+        let auth_type = "Bearer";
+        let server = MockServer::start();
+        let mock = server.mock(|when, then| {
+            when.method(POST)
+                .path(url_part)
+                .body_contains(model_name)
+                .body_contains(*inputs.get(0).unwrap())
+                .body_contains(*inputs.get(1).unwrap())
+                .body_contains(*inputs.get(2).unwrap())
+                .header("Authorization", format!("{} {}", auth_type, api_key));
+
+            then.status(200).body(encode_response);
+        });
+        let config = OpenAIEmbeddingsConfig {
+            url: format!("http://{}:{}{}", server.host(), server.port(), url_part),
+            model: model_name.to_string(),
+            api_key: Some(api_key.to_string()),
+            authorization_type: auth_type.to_string(),
+        };
+        let driver = OpenAIEmbeddingsDriver::new(&config).unwrap();
+        let actual_response = driver.encode_many(&inputs).await.unwrap();
+
+        mock.assert();
+        assert_eq!(
+            actual_response,
+            response
+                .data
+                .iter()
+                .map(|d| d.embedding.clone())
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_encode_batch_response_failed() {
+        let error_response = r#"{"error": "test"}"#;
+        let url_part = "/v1/embeddings";
+        let model_name = "test-model-name";
+        let inputs = vec!["test-input-1", "test-input-2", "test-input-3"];
+        let api_key = "api-key";
+        let auth_type = "Bearer";
+        let server = MockServer::start();
+        let mock = server.mock(|when, then| {
+            when.method(POST)
+                .path(url_part)
+                .body_contains(model_name)
+                .body_contains(*inputs.get(0).unwrap())
+                .body_contains(*inputs.get(1).unwrap())
+                .body_contains(*inputs.get(2).unwrap())
+                .header("Authorization", format!("{} {}", auth_type, api_key));
+
+            then.status(500).body(error_response);
+        });
+        let config = OpenAIEmbeddingsConfig {
+            url: format!("http://{}:{}{}", server.host(), server.port(), url_part),
+            model: model_name.to_string(),
+            api_key: Some(api_key.to_string()),
+            authorization_type: auth_type.to_string(),
+        };
+        let driver = OpenAIEmbeddingsDriver::new(&config).unwrap();
+        let response = driver.encode_many(&inputs).await;
+
+        mock.assert();
+        let expected_err = format!("HTTP 500 Internal Server Error: {}", error_response);
+        assert_eq!(response.unwrap_err().to_string(), expected_err);
+    }
+
+    #[tokio::test]
+    async fn test_encode_batch_parse_failed() {
+        let bad_response = r#"}{"#;
+        let url_part = "/v1/embeddings";
+        let model_name = "test-model-name";
+        let inputs = vec!["test-input-1", "test-input-2", "test-input-3"];
+        let api_key = "api-key";
+        let auth_type = "Bearer";
+        let server = MockServer::start();
+        let mock = server.mock(|when, then| {
+            when.method(POST)
+                .path(url_part)
+                .body_contains(model_name)
+                .body_contains(*inputs.get(0).unwrap())
+                .body_contains(*inputs.get(1).unwrap())
+                .body_contains(*inputs.get(2).unwrap())
+                .header("Authorization", format!("{} {}", auth_type, api_key));
+
+            then.status(200).body(bad_response);
+        });
+        let config = OpenAIEmbeddingsConfig {
+            url: format!("http://{}:{}{}", server.host(), server.port(), url_part),
+            model: model_name.to_string(),
+            api_key: Some(api_key.to_string()),
+            authorization_type: auth_type.to_string(),
+        };
+        let driver = OpenAIEmbeddingsDriver::new(&config).unwrap();
+        let response = driver.encode_many(&inputs).await;
+
+        mock.assert();
+
+        let expected_err = format!("HTTP 200 OK, failed to parse: {}", bad_response);
+        assert_eq!(response.unwrap_err().to_string(), expected_err);
+    }
 
     #[tokio::test]
     async fn test_encode() {
