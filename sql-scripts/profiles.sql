3888e6746f2a7902ddc40dffac3b6f77a6d25c40
Ian Kinsey <ian@aikbix.com>
2025-11-22 01:36:28 -0500

    Update profiles

--- sql-scripts/profiles.sql
+++ sql-scripts/profiles.sql
@@ -1,3 +1,7 @@
+--------------------------------------------------------------------------------
+-- Stub config
+--------------------------------------------------------------------------------
+
 CREATE OR REPLACE FUNCTION sqlgen.stub_config(
     instruct_output TEXT  DEFAULT NULL,
     encode_output   REAL[] DEFAULT NULL
@@ -15,3 +19,56 @@ SELECT jsonb_strip_nulls(
     )
 )::TEXT;
     $$;
+
+--------------------------------------------------------------------------------
+-- OpenAI completions config
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION sqlgen.openai_completions_config(
+    url                TEXT DEFAULT NULL,
+    model              TEXT DEFAULT NULL,
+    authorization_type TEXT DEFAULT NULL,
+    api_key            TEXT DEFAULT NULL
+)
+RETURNS TEXT
+LANGUAGE SQL
+AS $$
+SELECT jsonb_strip_nulls(
+    jsonb_build_object(
+        'type',   'OpenAICompletions',
+        'config', jsonb_build_object(
+            'url',                url,
+            'model',              model,
+            'authorization_type', authorization_type,
+            'api_key',            api_key
+        )
+    )
+)::TEXT;
+$$;
+
+--------------------------------------------------------------------------------
+-- OpenAI embeddings config
+--------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION sqlgen.openai_embeddings_config(
+    url                TEXT DEFAULT NULL,
+    model              TEXT DEFAULT NULL,
+    authorization_type TEXT DEFAULT NULL,
+    api_key            TEXT DEFAULT NULL
+)
+RETURNS TEXT
+LANGUAGE SQL
+AS $$
+SELECT jsonb_strip_nulls(
+    jsonb_build_object(
+        'type',   'OpenAIEmbeddings',
+        'config', jsonb_build_object(
+            'url',                url,
+            'model',              model,
+            'authorization_type', authorization_type,
+            'api_key',            api_key
+        )
+    )
+)::TEXT;
+$$;
