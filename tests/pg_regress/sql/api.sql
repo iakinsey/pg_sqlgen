991a9d81361d7853183af50d19344d8bf54a9434
Ian Kinsey <ian@aikbix.com>
2025-11-27 01:15:41 -0500

    Update regression tests

--- tests/pg_regress/sql/api.sql
+++ tests/pg_regress/sql/api.sql
@@ -1,34 +1,44 @@
--- Create model
+-- Create model.
 SELECT sqlgen.add_model('stub_model', sqlgen.stub_config('{"query": "SELECT 1;"}', ARRAY[1.1, 1.2, 2.3, 3.4]::real[]));
 
--- Verify model exists
+-- Verify model exists.
 SELECT * FROM sqlgen.models;
 
--- Create engine
+-- Create engine.
 SELECT sqlgen.create_engine('stub_engine', 'stub_model', 'stub_model');
 
--- Verify engine exists
+-- Verify engine exists.
 SELECT * FROM sqlgen.engines;
 
--- Set default engine
+-- Set default engine.
 SELECT sqlgen.set_default_engine('stub_engine');
 
--- Execute query via cursor with default engine
+-- Create a new table to verify triggers don't fail.
+CREATE TABLE t(x int);
+
+-- Alter the table to verify triggers don't fail.
+ALTER TABLE t ADD COLUMN y int;
+
+-- Add a new comment to verify triggers don't fail.
+COMMENT ON TABLE t IS 'test';
+
+-- Execute query via cursor with default engine.
 SELECT sqlgen.generate('A default stub query');
 
--- Execute query via cursor with engine parameter
+-- Execute query via cursor with engine parameter.
 SELECT sqlgen.generate('Another stub query', 'stub_engine');
 
--- Delete engine
+-- Delete engine.
 SELECT sqlgen.remove_engine('stub_engine');
 
--- Verify engine no longer exists
+-- Verify engine no longer exists.
 SELECT * FROM sqlgen.engines;
 
--- Delete model
+-- Delete model.
 SELECT sqlgen.remove_model('stub_model');
 
--- Verify model no longer exists
+-- Verify model no longer exists.
 SELECT * FROM sqlgen.models;
 
--- TODO add new data and make sure it reflects internals somehow
+-- Drop the table to verify triggers don't fail.
+DROP TABLE t;
