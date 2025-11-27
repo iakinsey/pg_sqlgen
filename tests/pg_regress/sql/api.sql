79dc21aa7e9216cd3f4e9c72814b01462bd7256b
Ian Kinsey <ian@aikbix.com>
2025-11-27 01:08:29 -0500

    Regression tests work

--- tests/pg_regress/sql/api.sql
+++ tests/pg_regress/sql/api.sql
@@ -1,32 +1,34 @@
 -- Create model
-SELECT sqlgen.add_model('StubModel', sqlgen.stub_config('SELECT 1', ARRAY[1.1, 1.2, 2.3, 3.4]::real[]));
+SELECT sqlgen.add_model('stub_model', sqlgen.stub_config('{"query": "SELECT 1;"}', ARRAY[1.1, 1.2, 2.3, 3.4]::real[]));
 
 -- Verify model exists
 SELECT * FROM sqlgen.models;
 
 -- Create engine
-SELECT sqlgen.create_engine('StubEngine', 'StubModel', 'StubModel');
+SELECT sqlgen.create_engine('stub_engine', 'stub_model', 'stub_model');
 
 -- Verify engine exists
 SELECT * FROM sqlgen.engines;
 
 -- Set default engine
-SELECT sqlgen.set_default_engine('StubEngine');
+SELECT sqlgen.set_default_engine('stub_engine');
 
 -- Execute query via cursor with default engine
 SELECT sqlgen.generate('A default stub query');
 
 -- Execute query via cursor with engine parameter
-SELECT sqlgen.generate('Another stub query', 'StubEngine');
+SELECT sqlgen.generate('Another stub query', 'stub_engine');
 
 -- Delete engine
-SELECT sqlgen.remove_engine('StubEngine');
+SELECT sqlgen.remove_engine('stub_engine');
 
 -- Verify engine no longer exists
 SELECT * FROM sqlgen.engines;
 
 -- Delete model
-SELECT sqlgen.delete_model('StubModel');
+SELECT sqlgen.remove_model('stub_model');
 
 -- Verify model no longer exists
 SELECT * FROM sqlgen.models;
+
+-- TODO add new data and make sure it reflects internals somehow
