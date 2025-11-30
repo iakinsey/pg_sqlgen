7c369e3029ffd94e0af8d799adadb6a0e1c12b04
Ian Kinsey <ian@aikbix.com>
2025-11-30 14:33:45 -0500

    Deb packages successfully built

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,66 +1,68 @@
 #!/usr/bin/env bash
 set -e
 
-VERSION=$(grep -m1 '^version' Cargo.toml | sed 's/version *= *"\(.*\)"/\1/')
-v="$1"
-if [ -z "$v" ]; then
-    echo "usage: $0 <pg_version>"
-    exit 1
-fi
+BUILD_VERSION=$(grep -m1 '^version' Cargo.toml | sed 's/version *= *"\(.*\)"/\1/')
+VERSIONS=(14 15 16 17 18)
 
-name="pg${v}"
+for v in "${VERSIONS[@]}"; do
+    name="pg${v}"
 
-if cargo pgrx info pg-config "$name" >/dev/null 2>&1; then
-    echo "${name} already initialized"
-else
-    cargo pgrx init "--${name}" download
-fi
+    if cargo pgrx info pg-config "$name" >/dev/null 2>&1; then
+        echo "${name} already initialized"
+    else
+        cargo pgrx init "--${name}" download
+    fi
 
-PG_CONFIG_PATH=$(cargo pgrx info pg-config "$v")
+    PG_CONFIG_PATH=$(cargo pgrx info pg-config "$v")
 
-echo "cargo pgrx package \
-    --pg-config "$PG_CONFIG_PATH" \
-    --features "pg$v" \
+    echo "cargo pgrx package \
+    --pg-config \"$PG_CONFIG_PATH\" \
+    --features \"pg$v\" \
     --no-default-features"
 
-base="$(pwd)/target/release/sqlgen-pg${v}"
-
-SQLGEN_SO_PATH="$(find . -type f -name 'sqlgen.so' -print -quit)"
-SQLGEN_CONTROL_PATH="$(find . -type f -name 'sqlgen.control' -print -quit)"
-SQLGEN_SQL_PATH="$(find . -type f -name 'sqlgen--*.sql' -print -quit)"
-
-if [ -n "$SQLGEN_SO_PATH" ]; then
-    SQLGEN_SO_PATH="$(readlink -f "$SQLGEN_SO_PATH")"
-fi
-
-if [ -n "$SQLGEN_CONTROL_PATH" ]; then
-    SQLGEN_CONTROL_PATH="$(readlink -f "$SQLGEN_CONTROL_PATH")"
-fi
-
-if [ -n "$SQLGEN_SQL_PATH" ]; then
-    SQLGEN_SQL_PATH="$(readlink -f "$SQLGEN_SQL_PATH")"
-fi
-
-EXTENSION_DIR=./target/package-${v}/usr/share/postgresql/${v}/extension/
-LIB_DIR=./target/package-${v}/usr/lib/postgresql/${v}/lib/
-
-rm -rf ${EXTENSION_DIR}
-rm -rf ${LIB_DIR}
-
-mkdir -p ${EXTENSION_DIR}
-mkdir -p ${LIB_DIR}
-
-cp ${SQLGEN_CONTROL_PATH} ${EXTENSION_DIR}
-cp ${SQLGEN_SQL_PATH} ${EXTENSION_DIR}
-cp ${SQLGEN_SO_PATH} ${LIB_DIR}
+    cargo pgrx package \
+        --pg-config $PG_CONFIG_PATH \
+        --features pg$v \
+        --no-default-features
 
 
+    base="$(pwd)/target/release/sqlgen-pg${v}"
+
+    SQLGEN_SO_PATH="$(find ${base} -type f -name 'sqlgen.so' -print -quit)"
+    SQLGEN_CONTROL_PATH="$(find ${base} -type f -name 'sqlgen.control' -print -quit)"
+    SQLGEN_SQL_PATH="$(find ${base} -type f -name 'sqlgen--*.sql' -print -quit)"
+
+    if [ -n "$SQLGEN_SO_PATH" ]; then
+        SQLGEN_SO_PATH="$(readlink -f "$SQLGEN_SO_PATH")"
+    fi
+
+    if [ -n "$SQLGEN_CONTROL_PATH" ]; then
+        SQLGEN_CONTROL_PATH="$(readlink -f "$SQLGEN_CONTROL_PATH")"
+    fi
+
+    if [ -n "$SQLGEN_SQL_PATH" ]; then
+        SQLGEN_SQL_PATH="$(readlink -f "$SQLGEN_SQL_PATH")"
+    fi
+
+    EXTENSION_DIR=./target/package/usr/share/postgresql/${v}/extension/
+    LIB_DIR=./target/package/usr/lib/postgresql/${v}/lib/
+
+    rm -rf "${EXTENSION_DIR}"
+    rm -rf "${LIB_DIR}"
+
+    mkdir -p "${EXTENSION_DIR}"
+    mkdir -p "${LIB_DIR}"
+
+    cp "${SQLGEN_CONTROL_PATH}" "${EXTENSION_DIR}"
+    cp "${SQLGEN_SQL_PATH}" "${EXTENSION_DIR}"
+    cp "${SQLGEN_SO_PATH}" "${LIB_DIR}"
+done
+
+rm -rf pg-sqlgen_${BUILD_VERSION}_amd64.deb
 fpm \
     -s dir \
     -t deb \
     -n pg_sqlgen\
-    -v ${VERSION} \
-    -C target/package-18 \
+    -v ${BUILD_VERSION} \
+    -C target/package \
     .
-
-# TODO have it run for every major version
