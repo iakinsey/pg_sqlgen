560b5109107b04e3965fc244895ea512c498b80e
Ian Kinsey <ian@aikbix.com>
2025-12-14 01:13:42 -0600

    create_engine only takes an array of schemas

--- sql-scripts/api.sql
+++ sql-scripts/api.sql
@@ -40,46 +40,6 @@ CREATE OR REPLACE FUNCTION sqlgen.create_engine(
     name TEXT,
     instruct_model TEXT,
     encoder_model TEXT,
-    schema_name TEXT DEFAULT NULL,
-    table_filter_type TEXT DEFAULT 'smart',
-    column_filter_limit INTEGER DEFAULT NULL,
-    error_correction_rounds INTEGER DEFAULT NULL,
-    system_prompt_template TEXT DEFAULT NULL,
-    user_prompt_template TEXT DEFAULT NULL,
-    relevant_ddls_template TEXT DEFAULT NULL,
-    similar_queries_template TEXT DEFAULT NULL,
-    filter_ddls_template TEXT DEFAULT NULL,
-    syntax_correction_template TEXT DEFAULT NULL,
-    explain_query_template TEXT DEFAULT NULL
-)
-RETURNS VOID
-LANGUAGE sql
-AS $$
-    SELECT sqlgen_internal.create_engine_external(
-        name,
-        instruct_model,
-        encoder_model,
-        CASE
-            WHEN schema_name IS NULL THEN ARRAY[]::TEXT[]
-            ELSE ARRAY[schema_name]
-        END,
-        table_filter_type,
-        error_correction_rounds,
-        column_filter_limit,
-        system_prompt_template,
-        user_prompt_template,
-        relevant_ddls_template,
-        similar_queries_template,
-        filter_ddls_template,
-        syntax_correction_template,
-        explain_query_template
-    );
-$$;
-
-CREATE OR REPLACE FUNCTION sqlgen.create_engine(
-    name TEXT,
-    instruct_model TEXT,
-    encoder_model TEXT,
     schema_names TEXT [] DEFAULT NULL,
     table_filter_type TEXT DEFAULT 'smart',
     column_filter_limit INTEGER DEFAULT NULL,
